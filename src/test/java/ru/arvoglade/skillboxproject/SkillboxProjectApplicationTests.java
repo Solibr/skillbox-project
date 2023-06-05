47d4cc0303f50cc41f8f8842de25ec34e821822e
Artem Gashev <artem.gaschev2013@yandex.ru>
2023-06-05 19:24:02 +0500

    Project structure created

---
+++ src/test/java/ru/arvoglade/skillboxproject/SkillboxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arvoglade.skillboxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillboxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
