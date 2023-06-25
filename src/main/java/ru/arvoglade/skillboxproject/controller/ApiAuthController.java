aa975fb36b1d6ed6a2626607d9b7b75ebc222b42
Artem Gashev <artem.gaschev2013@yandex.ru>
2023-06-25 19:34:28 +0500

    Controllers created

---
+++ src/main/java/ru/arvoglade/skillboxproject/controller/ApiAuthController.java
@@ -0,0 +1,7 @@
+package ru.arvoglade.skillboxproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("/api/auth")
+public class ApiAuthController {
+}
---
+++ src/main/java/ru/arvoglade/skillboxproject/controller/ApiPostController.java
@@ -0,0 +1,7 @@
+package ru.arvoglade.skillboxproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("/api/post")
+public class ApiPostController {
+}
