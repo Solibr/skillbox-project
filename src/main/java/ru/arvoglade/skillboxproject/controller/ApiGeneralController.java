d95d45ed4aaae9e9d043605324f1f908238e1279
Artem Gashev <artem.gaschev2013@yandex.ru>
2023-06-29 17:06:13 +0500

    Request mapping definition fixed

--- src/main/java/ru/arvoglade/skillboxproject/controller/ApiGeneralController.java
+++ src/main/java/ru/arvoglade/skillboxproject/controller/ApiGeneralController.java
@@ -1,7 +1,10 @@
 package ru.arvoglade.skillboxproject.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/api")
+@RestController
+@RequestMapping("/api")
 public class ApiGeneralController {
+
 }
